de148b15b57e92ec2f75242cc646aba70ac43c6c
joaobug <joaoenfrance@gmail.com>
2024-08-29 14:54:37 -0300

    Imported all countries flags to assets Designed a simple UI with a Zstack and two vstacks that hold the question and the flags, the flags are shown using a foreach 0..<3 we ask the question using the correctAnswer property, like gues the flag of Fance? France is the correctAnswer which is actually a random number between 0 and 2.... this random number is put in the array which has the shuffled modifier, there fore it is always giving us a different random question
    The foreach uses the same numbers 0 to 2 so the correctAnswer flag will always be there. When a flag is clicked a function is processed, this function takes the number of whichever the user clicked and compares it if it equals the correctAsnwer
    
    Finally we added the .alert modifier, which will show up when showingScore is toggled

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["US", "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Spain", "UK", "Ukraine"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var yourScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack{
+                    Text("Guess the flag of")
+                        .foregroundStyle(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+            ForEach(0..<3)  {  number in
+                Button {
+                    flagTapped(number)
+                } label: {
+                    Image(countries[number])
+                }
+            }  }
+            
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is \(yourScore)")
+        }
+    }
+    func flagTapped(_ number: Int){
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            yourScore += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
